13ab5ac9fd8fc81f22ccf3e3f2f0cde2bbe427b7
Дмитрий Афанасьев <dima-afa@bk.ru>
2018-07-22 14:20:03 +0400

    Оптимизирует (как-бы) код в файле headerNavigation.js

--- js/headerNavigation.js
+++ js/headerNavigation.js
@@ -5,45 +5,45 @@ window.onload = function() {
     var menuListBlock = document.querySelector(".navigation__section-menu");
     var menuList = document.querySelector("#js-nav-list");
     var navigationSection = document.querySelector("[data-navigation]");
-    buttonNavigation.classList.remove("navigation__button_hidden");
-    menuListBlock.classList.add("navigation__section-menu_hidden");
-    if(window.screen.width <= 1366) {
-        menuList.classList.add("navigation__list_js_true");
-    }
+
+    startRessetClass();
+
     buttonNavigation.addEventListener("click", function(e) {
-        e.preventDefault();
-        if (checkButtonNavigation) {
-            checkButtonNavigation = false;
-            menuListBlock.classList.add("navigation__section-menu_hidden");
-            focusManager.release(buttonNavigation);
-        } else {
-            checkButtonNavigation = true;
-            menuListBlock.classList.remove("navigation__section-menu_hidden");
-        }
+        toggleNavigationMenu(e, false);
     });
     buttonNavigation.addEventListener('keydown', function(e) {
         if(e.keyCode === 13 || e.keyCode === 32) {
-            if (checkButtonNavigation) {
-                e.preventDefault();
-                checkButtonNavigation = false;
-                menuListBlock.classList.add("navigation__section-menu_hidden");
-                focusManager.release(buttonNavigation);
-            } else {
-                e.preventDefault();
-                checkButtonNavigation = true;
-                menuListBlock.classList.remove("navigation__section-menu_hidden");
-                focusManager.capture(navigationSection);
-            }
+            toggleNavigationMenu(e, true);
         }
     });
     window.addEventListener("keydown", function(e) {
         if(e.keyCode === 27) {
-            if (checkButtonNavigation) {
-                e.preventDefault();
-                checkButtonNavigation = false;
-                menuListBlock.classList.add("navigation__section-menu_hidden");
-                focusManager.release(buttonNavigation);
-            }
+            toggleNavigationMenu(e, true);
         }
     });
+
+    function toggleNavigationMenu(e, checkFocus) {
+        if (checkButtonNavigation) {
+            e.preventDefault();
+            checkButtonNavigation = false;
+            menuListBlock.classList.add("navigation__section-menu_hidden");
+            if (checkFocus) {
+                focusManager.release(buttonNavigation);
+            }
+        } else {
+            e.preventDefault();
+            checkButtonNavigation = true;
+            menuListBlock.classList.remove("navigation__section-menu_hidden");
+            if (checkFocus) {
+                focusManager.capture(navigationSection);
+            }
+        }
+    }
+    function startRessetClass() {
+        buttonNavigation.classList.remove("navigation__button_hidden");
+        menuListBlock.classList.add("navigation__section-menu_hidden");
+        if(window.screen.width <= 1366) {
+            menuList.classList.add("navigation__list_js_true");
+        }
+    }
 };
